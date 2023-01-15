a2882921eaf650448b478ae0969782871e47a986
George Gkoutzomitros <georgegkoutzomitros@gmail.com>
2023-01-15 16:33:35 +0200

    task5 changes

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -12,5 +12,6 @@ task2.SumProduct();
 Task3 task3 = new Task3();
 task3.PrimeNumbers();
 
-
+Task5 task5 = new Task5();
+task5.SortNumbers();
 Console.ReadLine();
---
+++ Session-05/Session-05/Task5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+	internal class Task5
+	{
+		public void SortNumbers(){
+
+			int[] arr = new int[] { 0, -2, 1, 20, -31, 50, -4, 17, 89, 100 };
+
+			Array.Sort(arr);
+			Console.WriteLine("Ascending: ");
+			foreach (int value in arr)
+			{
+				Console.Write(value + " ");
+			}
+
+		}
+	}
+}
