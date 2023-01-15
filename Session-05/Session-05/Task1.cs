c08f26fa2ac91a70f8ded436513b2f4d8d248851
George Gkoutzomitros <georgegkoutzomitros@gmail.com>
2023-01-15 13:33:11 +0200

    changes task1

---
+++ Session-05/Session-05/Task1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05;
+	public class TaskA
+	{
+		public string ReverseName()
+		{
+			string name1 = "George";
+			string reverseString = string.Empty;
+
+			for (int i = name1.Length - 1; i >= 0; i--)
+			{
+				reverseString += name1[i];
+			}
+
+			return reverseString;
+
+
+		}
+
+	}
+
