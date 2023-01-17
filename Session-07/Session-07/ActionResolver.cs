622a53e8275ac4e97990c7ebd5b7a556af29066e
George Gkoutzomitros <georgegkoutzomitros@gmail.com>
2023-01-17 16:21:11 +0200

    first steps of creating classes

---
+++ Session-07/Session-07/ActionResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_07
+{
+	public class ActionResolver
+	{
+		// PROPERTIES
+		public MessageLogger Logger { get; set; }
+
+		// CTOR
+		public ActionResolver()
+		{
+
+		}
+
+		// METHODS
+		public ActionResponse Execute(ActionRequest request)
+		{
+
+
+			return null;
+		}
+	}
+}
